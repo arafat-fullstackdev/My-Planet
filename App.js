e3fe30eb75ce63819e37933f67fcd187f4ce98f7
Arafat Patwary <farabipsk@gmail.com>
2024-12-09 09:50:16 +0600

    Hello Planet Project

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize:'2.2rem'}}>Hello Planet!</Text>
+      <Text style={{color:'blue', fontSize:'2rem'}}>Let's Build Better Planet!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
